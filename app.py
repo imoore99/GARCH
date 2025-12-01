217625bf579f6760d8f79f0fa8c279ac9105e3b0
Ian Moore <snakelyfe9000@gmail.com>
2025-12-01 11:42:57 -0800

    Updated app.py for railway deployment

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import datetime
 import pytz
 from scipy import stats
 from io import StringIO
+import os
 
 # Page config
 st.set_page_config(
@@ -27,9 +28,9 @@ st.subheader("Automated SPY Risk Analytics")
 def load_data_from_s3():
     s3 = boto3.client(
         's3',
-        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
-        region_name=st.secrets["AWS_DEFAULT_REGION"]
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        region_name=os.environ["AWS_DEFAULT_REGION"]
     )
     
     # Read GARCH forecasts
